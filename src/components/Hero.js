86665de7ef7d697ff5bff19d007376fb54e7a948
togay-aytemiz <togayaytemiz@gmail.com>
2024-02-10 21:56:35 +0300

    solve bugs on hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,18 +11,18 @@ const Hero = () => {
           Grow with data.
         </h1>
         <div className="flex justify-center items-center gap-2">
-          <p className="md: text-4xl sm:text-3xl text-xl font-bold">
+          <p className="md:text-4xl sm:text-3xl text-xl font-bold">
             Fast, flexible financing for
           </p>
           <ReactTyped
-            className="md: text-4xl sm:text-3xl text-xl font-bold"
+            className="md:text-4xl sm:text-3xl text-xl font-bold"
             strings={["BTB", "BTC", "SAAS"]}
             typeSpeed={120}
             backSpeed={130}
             loop
           />
         </div>
-        <p className="md: text-2xl text-xl font-bold text-gray-600 mx-8">
+        <p className="md:text-2xl text-xl font-bold text-gray-600 mx-8">
           Monitor your data analytics to increase revenue for BTB, BTC and SAAS
           platforms.
         </p>
